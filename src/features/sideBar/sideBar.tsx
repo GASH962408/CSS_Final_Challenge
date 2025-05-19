e69fcc51f8c5c43f01e24840766819f7d71ebc1c
Gustavo <gustavosalazar@ravn.co>
2025-05-19 17:13:08 -0500

    feat(sideBar):Togle button create new

--- src/features/sideBar/sideBar.tsx
+++ src/features/sideBar/sideBar.tsx
@@ -1,19 +1,21 @@
 import "./sideBar.css"
 import SideBarMenu from "./SideBarMenu";
 import ListPortal from "./ListPortal";
+import { useState } from "react";
 
 
 export default function Sidebar() {
+  const [ListPort, setListPort] = useState(false)
   return (
     <aside className="sidebar">
       <section className="sidebar__top">
         <div className="sidebar__logo">
         </div>
       </section>
-      <SideBarMenu/>
+      <SideBarMenu />
       <section className="sidebar__footer">
-        <ListPortal/>
-        <button className="sidebar__create-button">
+        {ListPort && <ListPortal /> }
+        <button className="sidebar__create-button" onClick={() => setListPort(prev => !prev)}>
           <span className="sidebar__create-text">Create New</span>
           <span className="sidebar__create-icon">+</span>
         </button>
